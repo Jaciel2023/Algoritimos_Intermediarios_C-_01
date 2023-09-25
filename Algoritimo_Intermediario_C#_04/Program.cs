6d69cd5f5ddb22bdc1df5161d9fdfd120a251ada
Jaciel2023 <jacielnino2@gmail.com>
2023-09-25 18:58:57 -0300

    Teste de envio

--- Algoritimo_Intermediario_C#_04/Program.cs
+++ Algoritimo_Intermediario_C#_04/Program.cs
@@ -1,12 +1,13 @@
-﻿    Int32 n, soma;
+﻿    Int32 n;
+    decimal soma=0;
     {
     Console.Write("Digite um valor inteiro e positivo: ");
     n = Convert.ToInt32(Console.ReadLine());
 
-         soma = 0.0;
+         soma = 0;
         for (int i = 1; i <= n; i++)
         {
-            soma += 1.0 / i;
+            soma += 1 / i;
         }
 
         Console.WriteLine("A soma é: " + soma);
---
+++ Algoritimos_Intermediario_C#_07/Program.cs
@@ -0,0 +1,17 @@
+﻿Int32 numero, sequencia, final = 1;
+
+Console.WriteLine("Digite um número positivo e inteiro: N");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero == 0)
+{
+    Console.WriteLine("0! =1");
+}
+else
+{
+    for (sequencia = 1; sequencia <= numero; sequencia++)
+    {
+        final *= sequencia;
+    }
+    Console.WriteLine(numero + "! =" + final);
+}
